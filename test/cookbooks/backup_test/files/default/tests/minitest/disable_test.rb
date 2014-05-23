4696884c85add136c0f5b1a2999ff67ff4e81fe1
Scott M. Likens <scott@likens.us>
2014-05-22 23:57:13 -0700

    improve the testing suite

--- test/cookbooks/backup_test/files/default/tests/minitest/disable_test.rb
+++ test/cookbooks/backup_test/files/default/tests/minitest/disable_test.rb
@@ -4,11 +4,23 @@ describe "backup" do
     include MiniTest::Chef::Assertions
     include MiniTest::Chef::Context
     include MiniTest::Chef::Resources
-    it "should have a cron entry called 'archive_attribute_test'" do
+    it "should have /opt/backup" do
+      directory("/opt/backup").must_exist
+    end
+    it "should have /opt/backup/config.rb" do
+      file("/opt/backup/config.rb").must_exist
+    end
+    it "should not have /etc/cron.d/archive" do
       file("/etc/cron.d/archive").wont_exist
+    end
+    it "should not have a model for archive in /opt/backup/models" do
+      file("/opt/backup/models/archive.rb").wont_exist
     end
     it "should not have a file called /opt/backup/models/no_split_test.rb" do
       file("/opt/backup/models/no_split_test.rb").wont_exist
+    end
+    it "should not have /etc/cron.d/no_split_test" do
+      file("/etc/cron.d/no_split_test").wont_exist
     end
   end
 end
