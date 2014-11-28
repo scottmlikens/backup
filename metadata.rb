88441d525d19e153b160442edd8b3ab7bbc8d3ec
Scott M. Likens <scott@likens.us>
2014-11-27 17:43:01 -0800

    release 0.2.2

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "scott@likens.us"
 license          "Apache 2.0"
 description      "Installs/Configures backup"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.2.1"
+version          "0.2.2"
 
 %w{redhat centos fedora debian ubuntu}.each do |os|
   supports os
