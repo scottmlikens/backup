cd974e03635781385ecaf53c06c1ecc4c80aabf8
Scott M. Likens <scott@likens.us>
2017-05-29 20:26:38 -0700

    finish up the default test

--- test/integration/default/default_spec.rb
+++ test/integration/default/default_spec.rb
@@ -13,4 +13,54 @@ control 'backup should be installed and configured' do
     its(:content) { should match /tmp_path  \'\/tmp'/ }
     its(:content) { should match /root_path \'\/opt\/backup'/ }
   end
+  describe package('libxml2-dev') do
+    it { should be_installed }
+  end
+  describe package('libxslt1-dev') do
+    it { should be_installed }
+  end
+  # Our Crons should exist
+  describe file('/etc/cron.d/archive_attribute_test') do
+    it { should be_file }
+    its(:content) { should match /\/bin:\/usr\/bin:\/usr\/local\/bin:\/opt\/chef\/embedded\/bin/ }
+  end
+  describe file('/etc/cron.d/archive_encrypted') do
+    it { should be_file }
+  end
+  describe file('/etc/cron.d/archive') do
+    it { should be_file }
+  end
+  describe file('/etc/cron.d/no_split_test') do
+    it { should be_directory }
+  end
+    # this helps prove our tests tried to dump data
+  directory file('/tmp/archive') do
+    it { should be_directory }
+  end
+  directory file('/tmp/archive_encrypted') do
+    it { should be_directory }
+  end
+  describe file('/tmp/no_split_test') do
+    it { should be_directory }
+  end
+  describe file('/tmp/archive_attribute_test') do
+    it { should be_directory }
+  end
+end
+control 'it should have a models configured accordingly' do
+  describe file('/opt/backup/models/no_split_test.rb') do
+    it { should be_file )
+    its(:content) { should match /notify_by/ }
+    its(:content) { should match /split_into_chunks_of/ }
+  end
+  describe file('/opt/backup/models/archive_encrypted.rb') do
+    it { should be_file }
+    its(:content) { should match /encrypt_with OpenSSL/ }
+    its(:content) { should_not match /MAILTO/ }
+    its(:content) { should_not match /notify_by/ }
+  end
+  describe file('/opt/backup/models/archive_attribute_test.rb') do
+    it { should be_file }
+    its(:content) { should match /notify_by/ }
+  end
 end
