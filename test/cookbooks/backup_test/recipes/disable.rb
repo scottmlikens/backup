0ed0990e24dc809d7d8ebf632d1adf8dca4a2795
Scott M. Likens <scott@likens.us>
2017-06-02 17:20:48 -0700

    our test scaffolding will need ruby 2.3 for now

--- test/cookbooks/backup_test/recipes/disable.rb
+++ test/cookbooks/backup_test/recipes/disable.rb
@@ -16,6 +16,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+package "ruby-full" 
 
 backup_install node.name 
 backup_generate_config node.name
